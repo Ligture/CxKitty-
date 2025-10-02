0dcd4720eb9900159789f9b2c44973f22e1fff02
lpmon <2073377971@qq.com>
2025-10-02 20:13:30 +0800

    fix:session.py中添加代理设置 默认绕过系统代理

--- cxapi/session.py
+++ cxapi/session.py
@@ -1,4 +1,5 @@
 import re
+import sys
 import time
 from enum import Enum, auto
 from os import PathLike
@@ -20,6 +21,8 @@ from .exception import HandleCaptchaError
 from .schema import AccountInfo
 from .utils import get_ua
 from .face_detection import FaceDetectionDto
+sys.path.append("..")
+import config
 
 # 接口-获取验证码图片
 API_CAPTCHA_IMG = "https://mooc1-api.chaoxing.com/processVerifyPng.ac"
@@ -29,6 +32,10 @@ API_CAPTCHA_SUBMIT = "https://mooc1-api.chaoxing.com/html/processVerify.ac"
 
 ocr = DdddOcr(show_ad=False)
 
+if config.HTTP_EN:
+    proxies = {"http": config.HTTP,"https": config.HTTPS}
+else:
+    proxies = {"http": None, "https": None}  # 默认禁用代理 防止报错
 
 class SpecialPageType(Enum):
     """特殊页类型"""
@@ -189,6 +196,10 @@ class SessionWraper(Session):
         """
         self.__cb_resolve_face_before = cb
 
+    def post(self, *args, **kwargs) -> Response: #添加代理设置
+
+        return self.request(*args, **kwargs, proxies=proxies)
+
     def request(self, *args, **kwargs) -> Response:
         """ "requests.Session.request 的 hook 函数
         Args:
@@ -197,13 +208,13 @@ class SessionWraper(Session):
             Response: 响应数据
         """
         try:
-            resp = super().request(*args, **kwargs)
+            resp = super().request(*args, **kwargs,proxies=proxies)
         except requests.ConnectionError as e:
             self.__request_retry_cnt += 1
             self.logger.warning(f"连接错误 {e.__str__()}")
             time.sleep(self.__retry_delay)
             if self.__request_retry_cnt < self.__request_max_retry:
-                return self.request(*args, **kwargs)
+                return self.request(*args, **kwargs,proxies=proxies)
             else:
                 raise
         self.__request_retry_cnt = 0
@@ -212,7 +223,7 @@ class SessionWraper(Session):
                 # 验证码
                 self.__handle_anti_spider()
                 # 递归重发请求
-                resp = self.request(*args, **kwargs)
+                resp = self.request(*args, **kwargs,proxies=proxies)
                 return resp
 
             case SpecialPageType.FACE:
@@ -221,7 +232,7 @@ class SessionWraper(Session):
                 self.__handle_face_detection(resp)
 
                 # 递归重发请求
-                resp = self.request(*args, **kwargs)
+                resp = self.request(*args, **kwargs,proxies=proxies)
                 return resp
 
             case SpecialPageType.NORMAL:
@@ -308,7 +319,8 @@ class SessionWraper(Session):
                 "app": 0,
                 "ucode": code,
             },
-            allow_redirects=False,  # 阻止重定向，以进一步操作
+            allow_redirects=False,
+            proxies=proxies# 阻止重定向，以进一步操作
         )
 
         # HTTP 302 即验证正确，HTTP 202 即验证错误
